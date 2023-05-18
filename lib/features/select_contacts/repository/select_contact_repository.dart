9b76109644b9d161026161a5e7a905b4093f0e12
AkmenZ <akmenz89@gmail.com>
2023-05-18 23:18:46 +0100

    Customized bottom chat field

--- lib/features/select_contacts/repository/select_contact_repository.dart
+++ lib/features/select_contacts/repository/select_contact_repository.dart
@@ -4,7 +4,7 @@ import 'package:flutter_contacts/flutter_contacts.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:whatsapp_ui/common/utils/utils.dart';
 import 'package:whatsapp_ui/models/user_model.dart';
-import 'package:whatsapp_ui/screens/mobile_chat_screen.dart';
+import 'package:whatsapp_ui/features/chat/screens/mobile_chat_screen.dart';
 
 final selectContactsRepositoryProvider = Provider(
   (ref) => SelectContactRepository(firestore: FirebaseFirestore.instance),
@@ -36,10 +36,13 @@ class SelectContactRepository {
         var userData = UserModel.fromMap(document.data());
         String selectedPhoneNumber =
             selectedContact.phones[0].number.replaceAll('(', '').replaceAll(')', '').replaceAll(' ', '');
-            print(selectedPhoneNumber);
+            // print(selectedPhoneNumber);
         if (selectedPhoneNumber == userData.phoneNumber) {
           isFound = true;
-          Navigator.pushNamed(context, MobileChatScreen.routeName);
+          Navigator.pushNamed(context, MobileChatScreen.routeName, arguments: {
+            'name': userData.name,
+            'uid': userData.uid
+          });
         }
       }
       if (!isFound) {
--- lib/router.dart
+++ lib/router.dart
@@ -4,7 +4,7 @@ import 'package:whatsapp_ui/features/auth/screens/login_screen.dart';
 import 'package:whatsapp_ui/features/auth/screens/otp_screen.dart';
 import 'package:whatsapp_ui/features/auth/screens/user_info_screen.dart';
 import 'package:whatsapp_ui/features/select_contacts/screens/select_contacts_screen.dart';
-import 'package:whatsapp_ui/screens/mobile_chat_screen.dart';
+import 'package:whatsapp_ui/features/chat/screens/mobile_chat_screen.dart';
 
 Route<dynamic> generateRoute(RouteSettings settings) {
   switch (settings.name) {
@@ -17,17 +17,23 @@ Route<dynamic> generateRoute(RouteSettings settings) {
       return MaterialPageRoute(
         builder: (context) => OTPScreen(verificationId: verificationId),
       );
-      case UserInformationScreen.routeName:
+    case UserInformationScreen.routeName:
       return MaterialPageRoute(
         builder: (context) => const UserInformationScreen(),
       );
-      case SelectContactsScreen.routeName:
+    case SelectContactsScreen.routeName:
       return MaterialPageRoute(
         builder: (context) => const SelectContactsScreen(),
       );
-      case MobileChatScreen.routeName:
+    case MobileChatScreen.routeName:
+      final arguments = settings.arguments as Map<String, dynamic>;
+      final name = arguments['name'];
+      final uid = arguments['uid'];
       return MaterialPageRoute(
-        builder: (context) => const MobileChatScreen(),
+        builder: (context) => MobileChatScreen(
+          name: name,
+          uid: uid,
+        ),
       );
     default:
       // when no screens match
